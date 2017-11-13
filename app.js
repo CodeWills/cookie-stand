781d9b6f53d9a2c8c032d5cfc8d0cfe8795f74ba
Cody <williwonka14@gmail.com>
2017-11-13 15:30:05 -0800

    added all stores

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 'use strict';
+
+var hours = ['6 am','7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm','8 pm'];
+
+var firstAndPike = {
+  name:'1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+};
+
+var seaTac = {
+  name:'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+};
+
+var seattleCenter = {
+  name:'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgSale: 3.7,
+};
+
+var capHill = {
+  name:'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgSale: 2.3,
+};
+
+var Alki = {
+  name:'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgSale: 4.6,
+};
